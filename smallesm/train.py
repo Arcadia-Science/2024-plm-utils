8e0b743f24dc4d2b1251a677819ad61150891dfb
Keith Cheveralls <keith.chev@gmail.com>
2024-02-17 00:46:52 +0000

    some cleanup and renaming

--- smallesm/train.py
+++ smallesm/train.py
@@ -17,36 +17,34 @@ def load_and_filter_embeddings(embeddings_filepath, max_length=None):
     if max_length is None:
         return embeddings
 
-    # hack to get the peptides fasta file corresponding to the embeddings.
+    # TODO: a less hackish way to get the peptides fasta file corresponding to the embeddings.
     peptides_fasta_filepath = embeddings_filepath.with_name(
         embeddings_filepath.stem.replace("embeddings", "peptides.fa")
     )
 
-    # the indices of the embeddings to keep.
-    inds = [
+    filtered_inds = [
         ind
         for ind, record in enumerate(SeqIO.parse(peptides_fasta_filepath, "fasta"))
         if len(record.seq) <= max_length
     ]
+    return embeddings[filtered_inds, :]
 
-    return embeddings[inds, :]
 
-
-def load_data_and_labels(coding_filepath, noncoding_filepath, max_length=None):
+def load_embeddings_and_labels(coding_filepath, noncoding_filepath, max_length=None):
     """
-    Load embeddings from the given filepaths and create labels for the data.
+    Load embeddings from the given filepaths and create labels for coding/noncoding.
     """
-    x_coding = load_and_filter_embeddings(coding_filepath, max_length)
-    x_noncoding = load_and_filter_embeddings(noncoding_filepath, max_length)
+    embs_coding = load_and_filter_embeddings(coding_filepath, max_length)
+    embs_noncoding = load_and_filter_embeddings(noncoding_filepath, max_length)
 
-    # create labels for the data using 1 for coding and 0 for noncoding sequences.
-    labels_coding = np.ones(x_coding.shape[0])
-    labels_noncoding = np.zeros(x_noncoding.shape[0])
+    # create labels for the embeddings using 1 for 'coding' and 0 for 'noncoding'.
+    labels_coding = np.ones(embs_coding.shape[0])
+    labels_noncoding = np.zeros(embs_noncoding.shape[0])
 
-    X = np.concatenate([x_coding, x_noncoding], axis=0)
-    y = np.concatenate([labels_coding, labels_noncoding])
+    embs_all = np.concatenate([embs_coding, embs_noncoding], axis=0)
+    labels_all = np.concatenate([labels_coding, labels_noncoding])
 
-    return X, y
+    return embs_all, labels_all
 
 
 def train(
@@ -56,21 +54,31 @@ def train(
     noncoding_test_filepath=None,
     max_length=None,
 ):
-    X, y = load_data_and_labels(coding_train_filepath, noncoding_train_filepath, max_length)
+    """
+    Train a random forest classifier using embeddings of peptides from coding and noncoding
+    transcripts and print the validation and test metrics.
+
+    Nomenclature note: we follow the common ML convention of using `x` to denote
+    the matrix of input features and `y` to denote the labels we are trying to predict.
+    """
+
+    x_all, y_all = load_embeddings_and_labels(
+        coding_train_filepath, noncoding_train_filepath, max_length
+    )
 
     # use PCA to reduce the dimensionality of the data to make training faster.
     # (`n_components` was chosen empirically from a plot of the explained variance.)
     pca = sklearn.decomposition.PCA(n_components=30)
-    pca.fit(X)
+    pca.fit(x_all)
 
-    X_pcs = pca.transform(X)
+    x_pcs = pca.transform(x_all)
 
-    X_train, X_validation, y_train, y_validation = sklearn.model_selection.train_test_split(
-        X_pcs, y, test_size=0.2, random_state=RANDOM_STATE
+    x_train, x_validation, y_train, y_validation = sklearn.model_selection.train_test_split(
+        x_pcs, y_all, test_size=0.2, random_state=RANDOM_STATE
     )
 
-    # `n_estimators` and `min_samples_split` were chosen empirically for speed;
-    # perf doesn't improve much with more estimators or deeper trees.
+    # `n_estimators` and `min_samples_split` were chosen empirically for fast training,
+    # but perf doesn't improve much with more estimators or deeper trees.
     model = sklearn.ensemble.RandomForestClassifier(
         n_estimators=30,
         min_samples_split=10,
@@ -78,10 +86,10 @@ def train(
         n_jobs=-1,
         random_state=RANDOM_STATE,
     )
-    model.fit(X_train, y_train)
+    model.fit(x_train, y_train)
 
-    y_train_pred = model.predict(X_train)
-    y_validation_pred = model.predict(X_validation)
+    y_train_pred = model.predict(x_train)
+    y_validation_pred = model.predict(x_validation)
 
     train_metrics = calc_metrics(y_train, y_train_pred)
     pretty_print_metrics(train_metrics, header="Training metrics")
@@ -90,19 +98,19 @@ def train(
     pretty_print_metrics(validation_metrics, header="Validation metrics")
 
     if coding_test_filepath is not None and noncoding_test_filepath is not None:
-        X_test, y_test = load_data_and_labels(
+        x_test, y_test = load_embeddings_and_labels(
             coding_test_filepath, noncoding_test_filepath, max_length
         )
 
-        X_test_pcs = pca.transform(X_test)
-        y_test_pred = model.predict(X_test_pcs)
+        x_test_pcs = pca.transform(x_test)
+        y_test_pred = model.predict(x_test_pcs)
         test_metrics = calc_metrics(y_test, y_test_pred)
         pretty_print_metrics(test_metrics, header="Test metrics")
 
 
 def calc_metrics(y_true, y_pred):
     """
-    Calculate precision, recall, and F1 score for the given true and predicted labels.
+    Calculate performance metrics for the given true and predicted labels.
     """
     accuracy = sklearn.metrics.accuracy_score(y_true, y_pred)
     precision = sklearn.metrics.precision_score(y_true, y_pred)
