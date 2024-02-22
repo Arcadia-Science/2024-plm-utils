b77846b518d26716973ced829a1e22a4875eeece
Keith Cheveralls <keith.chev@gmail.com>
2024-02-22 04:08:43 +0000

    add quick sript to train on one species and test on another (WIP)

--- smallesm/cli.py
+++ smallesm/cli.py
@@ -1,3 +1,5 @@
+import pathlib
+
 import click
 
 from smallesm import datasets, embed, train, translate
@@ -14,3 +16,23 @@ cli.add_command(embed.command, name="embed")
 cli.add_command(translate.command, name="translate")
 cli.add_command(train.command, name="train")
 cli.add_command(datasets.cli, name="datasets")
+
+
+@cli.command()
+@click.argument("dirname_suffix", type=str, required=True)
+@click.option("--train-id", type=str, required=True)
+@click.option("--test-id", type=str, required=True)
+def train_test(dirname_suffix, train_id, test_id):
+    """
+    Train and test a model using the given species.
+    """
+    dirpath = pathlib.Path("tmp/data/")
+    coding_dirpath = dirpath / f"cdna-clustered-{dirname_suffix}"
+    noncoding_dirpath = dirpath / f"ncrna-clustered-{dirname_suffix}"
+
+    train.train(
+        coding_train_filepath=coding_dirpath / f"{train_id}.npy",
+        noncoding_train_filepath=noncoding_dirpath / f"{train_id}.npy",
+        coding_test_filepath=coding_dirpath / f"{test_id}.npy",
+        noncoding_test_filepath=noncoding_dirpath / f"{test_id}.npy",
+    )
