cc2e4f92ff1f1cf6050c5ea1f3aa19e4c537188e
Keith Cheveralls <keith.chev@gmail.com>
2024-03-05 01:52:26 +0000

    rename classifier module to classify for consistency

--- plmutils/cli.py
+++ plmutils/cli.py
@@ -1,6 +1,6 @@
 import click
 
-from plmutils import classifier, embed, translate
+from plmutils import classify, embed, translate
 from plmutils.tasks import orf_prediction
 
 
@@ -13,6 +13,6 @@ def cli(ctx):
 
 cli.add_command(embed.command, name="embed")
 cli.add_command(translate.command, name="translate")
-cli.add_command(classifier.train_command, name="train")
-cli.add_command(classifier.predict_command, name="predict")
+cli.add_command(classify.train_command, name="train")
+cli.add_command(classify.predict_command, name="predict")
 cli.add_command(orf_prediction.cli, name="orf-prediction")
